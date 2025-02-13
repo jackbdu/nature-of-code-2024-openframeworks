6440c923000e66db011ceece7f286f29c6c7e428
Jack B. Du <github@jackbdu.com>
2025-02-12 21:53:53 -0500

    add 1_4

---
+++ Example_1_4_Vector_Multiplication/src/ofApp.cpp
@@ -0,0 +1,65 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {}
+
+//--------------------------------------------------------------
+void ofApp::update() { ofBackground(255); }
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  ofVec2f mouse = ofVec2f(ofGetMouseX(), ofGetMouseY());
+  ofVec2f center = ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2);
+  mouse -= center;
+
+  ofTranslate(center.x, center.y);
+
+  ofSetLineWidth(2);
+  ofSetColor(200);
+  ofDrawLine(0, 0, mouse.x, mouse.y);
+
+  mouse *= 0.5;
+
+  ofSetColor(0);
+  ofSetLineWidth(4);
+  ofDrawLine(0, 0, mouse.x, mouse.y);
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
