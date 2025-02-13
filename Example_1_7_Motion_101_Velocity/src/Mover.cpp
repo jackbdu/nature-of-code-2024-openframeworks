8cc61f84f31f563977f4a91b4d67000327e7cbf2
Jack B. Du <github@jackbdu.com>
2025-02-12 22:13:17 -0500

    add 1_7

---
+++ Example_1_7_Motion_101_Velocity/src/Mover.cpp
@@ -0,0 +1,35 @@
+#include "Mover.h"
+
+void Mover::setup() {
+  position = ofVec2f{ofRandomWidth(), ofRandomHeight()};
+  velocity = ofVec2f{ofRandom(-2, 2), ofRandom(-2, 2)};
+}
+
+void Mover::update() { position += velocity; }
+
+void Mover::show() {
+  const float r = 24;
+
+  ofFill();
+  ofSetColor(127);
+  ofDrawCircle(position.x, position.y, r);
+
+  ofNoFill();
+  ofSetColor(0);
+  ofSetLineWidth(2);
+  ofDrawCircle(position.x, position.y, r);
+}
+
+void Mover::checkEdges() {
+  if (position.x > ofGetWidth()) {
+    position.x = 0;
+  } else if (position.x < 0) {
+    position.x = ofGetWidth();
+  }
+
+  if (position.y > ofGetHeight()) {
+    position.y = 0;
+  } else if (position.y < 0) {
+    position.y = ofGetHeight();
+  }
+}
---
+++ Example_1_7_Motion_101_Velocity/src/Mover.h
@@ -0,0 +1,17 @@
+#ifndef _MOVER
+#define _MOVER
+
+#include "ofMain.h"
+
+class Mover {
+public:
+  void setup();
+  void update();
+  void show();
+  void checkEdges();
+
+  ofVec2f position;
+  ofVec2f velocity;
+};
+
+#endif
