376af2e97420d3850838a3e345501d0f65ea11ff
Jack B. Du <github@jackbdu.com>
2025-02-12 21:16:54 -0500

    add 0_1

---
+++ Example_0_1_Random_Walk/src/Walker.cpp
@@ -0,0 +1,24 @@
+#include "Walker.h"
+
+void Walker::setup() {
+  x = ofGetWidth() * 0.5;
+  y = ofGetHeight() * 0.5;
+}
+
+void Walker::show() {
+  ofSetColor(0);
+  ofDrawRectangle(x, y, 1, 1);
+}
+
+void Walker::step() {
+  const int choice = ofRandom(4);
+  if (choice == 0) {
+    x++;
+  } else if (choice == 1) {
+    x--;
+  } else if (choice == 2) {
+    y++;
+  } else {
+    y--;
+  }
+}
