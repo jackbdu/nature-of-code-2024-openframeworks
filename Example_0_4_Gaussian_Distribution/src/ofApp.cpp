7b70c666795c734b6cf44aafedbeaeb83f3a32cb
Jack B. Du <github@jackbdu.com>
2025-02-12 21:26:32 -0500

    add 0_4

---
+++ Example_0_4_Gaussian_Distribution/src/ofApp.cpp
@@ -0,0 +1,56 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  ofSetBackgroundAuto(false);
+  ofBackground(255);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  const float x = ofRandomGaussian(320.0, 60.0);
+  ofSetColor(0, 10);
+  ofDrawCircle(x, 120, 8);
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
