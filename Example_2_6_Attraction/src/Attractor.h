159f26b2c038d1c0fab62b13f5173e3cd542fe5d
Jack B. Du <github@jackbdu.com>
2025-02-15 05:23:42 -0500

    add 2_6

---
+++ Example_2_6_Attraction/src/Attractor.h
@@ -0,0 +1,24 @@
+#ifndef _ATTRACTOR
+#define _ATTRACTOR
+
+#include "Mover.h"
+#include "ofMain.h"
+
+class Attractor {
+public:
+  void setup();
+  void show();
+  void handlePress(float mx, float my);
+  void handleHover(float mx, float my);
+  void handleDrag(float mx, float my);
+  void stopDragging();
+  ofVec2f attract(Mover mover, float G);
+
+  float mass;
+  ofVec2f position;
+  ofVec2f dragOffset;
+  bool dragging;
+  bool rollover;
+};
+
+#endif
---
+++ Example_2_6_Attraction/src/ofApp.cpp
@@ -0,0 +1,71 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  mover.setup(300, 50, 2);
+  attractor.setup();
+  G = 1;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+  const ofVec2f force = attractor.attract(mover, G);
+  mover.applyForce(force);
+  mover.update();
+
+  ofBackground(255);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  attractor.show();
+  mover.show();
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) { attractor.handleHover(x, y); }
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+  attractor.handleHover(x, y);
+  attractor.handleDrag(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+  attractor.handlePress(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+  attractor.stopDragging();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
