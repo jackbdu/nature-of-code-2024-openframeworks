a9a9b1fd4c0f24e206ae79afc455050f506c6936
Jack B. Du <github@jackbdu.com>
2025-02-16 19:21:22 -0500

    add 3_3

---
+++ Example_3_3_Pointing_In_The_Direction_Of_Motion/src/Mover.cpp
@@ -0,0 +1,59 @@
+#include "Mover.h"
+
+void Mover::setup() {
+  position = ofVec2f(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
+  velocity = ofVec2f(0, 0);
+  acceleration = ofVec2f(0, 0);
+  topSpeed = 4;
+}
+
+void Mover::update() {
+  ofVec2f mouse = ofVec2f(ofGetMouseX(), ofGetMouseY());
+  ofVec2f dir = mouse - position;
+
+  dir.normalize();
+
+  dir *= 0.5;
+
+  acceleration = dir;
+
+  velocity += acceleration;
+  velocity.limit(topSpeed);
+  position += velocity;
+}
+
+void Mover::show() {
+  const float angle = ofVec2f{1, 0}.angleRad(velocity);
+
+  ofPushMatrix();
+
+  ofTranslate(position.x, position.y);
+  ofRotateRad(angle);
+
+  ofSetRectMode(OF_RECTMODE_CENTER);
+
+  ofFill();
+  ofSetColor(127);
+  ofDrawRectangle(0, 0, 30, 10);
+
+  ofNoFill();
+  ofSetColor(0);
+  ofSetLineWidth(2);
+  ofDrawRectangle(0, 0, 30, 10);
+
+  ofPopMatrix();
+}
+
+void Mover::checkEdges() {
+  if (position.x > ofGetWidth()) {
+    position.x = 0;
+  } else if (position.x < 0) {
+    position.x = ofGetWidth();
+  }
+
+  if (position.y > ofGetHeight()) {
+    position.y = 0;
+  } else if (position.y < 0) {
+    position.y = ofGetHeight();
+  }
+}
