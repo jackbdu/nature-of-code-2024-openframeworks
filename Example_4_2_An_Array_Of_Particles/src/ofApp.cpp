cd1c1b9efd24049faf1163d5667a4c01b96a204e
Jack B. Du <github@jackbdu.com>
2025-02-20 15:27:43 -0500

    add 4_2

---
+++ Example_4_2_An_Array_Of_Particles/src/ofApp.cpp
@@ -0,0 +1,68 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+  Particle particle;
+  particle.setup(ofGetWidth() * 0.5, 20);
+  particles.push_back(particle);
+
+  for (int i = particles.size() - 1; i >= 0; --i) {
+    auto particle = &particles[i];
+    particle->run();
+    if (particle->isDead()) {
+      particles.erase(particles.begin() + i);
+    }
+  }
+
+  ofBackground(255);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  for (int i = particles.size() - 1; i >= 0; --i) {
+    auto particle = &particles[i];
+    particle->show();
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
