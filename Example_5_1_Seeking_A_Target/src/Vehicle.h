f8be8df89e89ed171dfab0d13bbde479f8ac0d9a
Jack B. Du <github@jackbdu.com>
2025-03-02 05:28:46 -0500

    add 5_1

---
+++ Example_5_1_Seeking_A_Target/src/Vehicle.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ofMain.h"
+
+class Vehicle {
+public:
+  void setup(float x, float y);
+  void update();
+  void applyForce(ofVec2f force);
+  void seek(ofVec2f target);
+  void show();
+
+  ofVec2f position;
+  ofVec2f velocity;
+  ofVec2f acceleration;
+  float r;
+  float maxSpeed;
+  float maxForce;
+};
---
+++ Example_5_1_Seeking_A_Target/src/ofApp.cpp
@@ -0,0 +1,68 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() { vehicle.setup(ofGetWidth() * 0.5, ofGetHeight() * 0.5); }
+
+//--------------------------------------------------------------
+void ofApp::update() {
+  ofBackground(255);
+
+  mouse = ofVec2f(ofGetMouseX(), ofGetMouseY());
+
+  vehicle.seek(mouse);
+  vehicle.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  ofSetLineWidth(2);
+
+  ofSetColor(127);
+  ofFill();
+  ofDrawCircle(mouse.x, mouse.y, 24);
+
+  ofSetColor(0);
+  ofNoFill();
+  ofDrawCircle(mouse.x, mouse.y, 24);
+
+  vehicle.show();
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
