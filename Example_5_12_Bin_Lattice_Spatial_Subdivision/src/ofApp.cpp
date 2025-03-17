606c8f1898db64ae1c53713e61dfebfb19a14948
Jack B. Du <github@jackbdu.com>
2025-03-17 18:01:06 -0400

    remove framerate display

--- Example_5_12_Bin_Lattice_Spatial_Subdivision/src/ofApp.cpp
+++ Example_5_12_Bin_Lattice_Spatial_Subdivision/src/ofApp.cpp
@@ -40,8 +40,6 @@ void ofApp::update() {
   }
 
   flock.update(&grid, resolution);
-
-  ofSetWindowTitle(ofToString(ofGetFrameRate()));
 }
 
 //--------------------------------------------------------------
