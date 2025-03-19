1f7097b8f70f588621904050bd27016eae19cc74
Jack B. Du <github@jackbdu.com>
2025-03-18 22:15:28 -0400

    add 8_2

---
+++ Example_8_2_Recursive_Circles_Twice/src/ofApp.cpp
@@ -0,0 +1,67 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  ofSetFrameRate(0);
+  ofSetCircleResolution(100);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() { ofBackground(255); }
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  drawCircles(ofGetWidth() * 0.5, ofGetHeight() * 0.5, 320);
+}
+
+//--------------------------------------------------------------
+void ofApp::drawCircles(float x, float y, float radius) {
+  ofSetLineWidth(2);
+  ofSetColor(0);
+  ofNoFill();
+  ofDrawCircle(x, y, radius);
+
+  if (radius > 4) {
+    drawCircles(x + radius * 0.5, y, radius * 0.5);
+    drawCircles(x - radius * 0.5, y, radius * 0.5);
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
