f827311e329f54b0117ec10b29918dcb0bf41e49
Jack B. Du <github@jackbdu.com>
2025-03-18 22:24:23 -0400

    add 8_4

---
+++ Example_8_4_The_Cantor_Set/src/ofApp.cpp
@@ -0,0 +1,65 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  ofSetFrameRate(0);
+  ofSetCircleResolution(100);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() { ofBackground(255); }
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  ofSetLineWidth(2);
+  cantor(10, 10, 620);
+}
+
+//--------------------------------------------------------------
+void ofApp::cantor(float x, float y, float length) {
+  ofSetColor(0);
+  if (length > 1) {
+    ofDrawLine(x, y, x + length, y);
+    cantor(x, y + 20, length / 3);
+    cantor(x + length * 2 / 3, y + 20, length / 3);
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
